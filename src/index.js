1b5ff39da651f34673804062b51bc16952ae5cfe
qiangmin <463070949@qq.com>
2018-04-25 16:02:55 +0800

    添加注释

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+// 重写了Vue对象
 import _ from './util';
 import installGlobalAPI from './global-api';
 
@@ -7,13 +8,21 @@ function Vue(options) {
 // js原型对象
 Vue.prototype = {
     constructor: Vue,
+    // 初始化实例
     ...require('./instance/init'),
+    // 渲染节点
     ...require('./instance/compile'),
+    // 初始化节点
     ...require('./instance/element'),
+    // 监听数据变化，computed，watch等
     ...require('./instance/bindings'),
+    // 初始化options
     ...require('./instance/scope'),
+    // 解析、渲染DOM
     ...require('./api/lifecycle'),
+    // 处理data
     ...require('./api/data'),
+    // 处理DOM
     ...require('./api/dom')
 };
 
